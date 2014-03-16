98f6630d519d035060ed0d4cc37600f5e692b2d8
shaohang <shao.hang523@gmail.com>
2014-03-16 00:05:41 -0400

    KL divergence version 0

---
+++ KL_divergence.m
@@ -0,0 +1,10 @@
+function kl_diverg = KL_divergence(dataset1,dataset2)
+
+%% Compute Entropy of dataset1 and dataset2
+
+[prob1 , entropy1] = entropy(dataset1);
+[prob2 , entropy2] = entropy(dataset2);
+
+%% Compute KL divergence based on entropy
+
+kl_diverg = entropy1-entropy2;
