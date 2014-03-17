2e508fd35263d89742bd17eb49d4428d1ec1e9d3
Jiasen Lu <echosenm@gmail.com>
2014-03-17 11:31:22 -0400

    main

--- main.m
+++ main.m
@@ -1,11 +1,12 @@
+clear all
 load DataCursive34Print1234.mat
-mean_cursive3 = sum(cursive3,1)/size(cursive3,1);
-mean_cursive4 = sum(cursive4,1)/size(cursive4,1);
+mean_cursive3 = mean(cursive3);
+mean_cursive4 = mean(cursive4);
 
-mean_print1 = sum(print1,1)/size(print1,1);
-mean_print2 = sum(print2,1)/size(print2,1);
-mean_print3 = sum(print3,1)/size(print3,1);
-mean_print4 = sum(print4,1)/size(print4,1);
+mean_print1 = mean(print1);
+mean_print2 = mean(print2);
+mean_print3 = mean(print3);
+mean_print4 = mean(print4);
 
 [prob_print1, entropy_print1] = entropy(print1);
 [prob_print2, entropy_print2] = entropy(print2);
@@ -16,19 +17,18 @@ mean_print4 = sum(print4,1)/size(print4,1);
 [prob_cursive4, entropy_cursive4] = entropy(cursive4);
 
 
-sampled_print1 = Gibbs(print1, 2000, 1000);
-sampled_print2 = Gibbs(print2, 2000, 1000);
-sampled_print3 = Gibbs(print3, 2000, 1000);
-sampled_print4 = Gibbs(print4, 2000, 1000);
+sampled_print1 = Gibbs(print1, 2000, 4000);
+sampled_print2 = Gibbs(print2, 2000, 4000);
+sampled_print3 = Gibbs(print3, 2000, 4000);
+sampled_print4 = Gibbs(print4, 2000, 4000);
 
-sampled_cursive3 = Gibbs(cursive3, 2000, 1000);
-sampled_cursive4 = Gibbs(cursive4, 2000, 1000);
+sampled_cursive3 = Gibbs(cursive3, 2000, 4000);
+sampled_cursive4 = Gibbs(cursive4, 2000, 4000);
 
 [prob_sampleprint1, entropy_sampleprint1] = entropy(sampled_print1);
 [prob_sampleprint2, entropy_sampleprint2] = entropy(sampled_print2);
 [prob_sampleprint3, entropy_sampleprint3] = entropy(sampled_print3);
 [prob_sampleprint4, entropy_sampleprint4] = entropy(sampled_print4);
 
-kl_diverg = entropy1-entropy2;
 
 
